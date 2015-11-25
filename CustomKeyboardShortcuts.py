8d468f0ded241ef4dce77b6f773629165d62c00d
Arthaey <arthaey@gmail.com>
2015-11-24 23:16:36 -0600

    Customize shortcuts for decks, overview, add, browse, stats, and sync.

---
+++ CustomKeyboardShortcuts.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# See github page to report issues or to contribute:
+# https://github.com/Arthaey/anki-custom-keyboard-shortcuts
+
+from aqt.qt import QMainWindow
+from aqt.main import AnkiQt
+
+GLOBAL_DECKS_SHORTCUT    = "d"
+GLOBAL_OVERVIEW_SHORTCUT = "s"
+GLOBAL_ADD_SHORTCUT      = "a"
+GLOBAL_BROWSE_SHORTCUT   = "b"
+GLOBAL_STATS_SHORTCUT    = "S"
+GLOBAL_SYNC_SHORTCUT     = "y"
+
+# Copy-pasted from aqt/main.py
+def _keyPressEvent(self, evt):
+    # do we have a delegate?
+    if self.keyHandler:
+        # did it eat the key?
+        if self.keyHandler(evt):
+            return
+    # run standard handler
+    QMainWindow.keyPressEvent(self, evt)
+    # check global keys
+    key = unicode(evt.text())
+    if key == GLOBAL_DECKS_SHORTCUT:
+        self.moveToState("deckBrowser")
+    elif key == GLOBAL_OVERVIEW_SHORTCUT:
+        if self.state == "overview":
+            self.col.startTimebox()
+            self.moveToState("review")
+        else:
+            self.moveToState("overview")
+    elif key == GLOBAL_ADD_SHORTCUT:
+        self.onAddCard()
+    elif key == GLOBAL_BROWSE_SHORTCUT:
+        self.onBrowse()
+    elif key == GLOBAL_STATS_SHORTCUT:
+        self.onStats()
+    elif key == GLOBAL_SYNC_SHORTCUT:
+        self.onSync()
+
+# Replaces the method entirely; wrapping would cause the customized shortcuts to
+# run *in addition to* the original shortcuts.
+AnkiQt.keyPressEvent = _keyPressEvent
---
+++ README.md
@@ -0,0 +1,28 @@
+# Description
+
+An Anki add-on that lets you customize the following global shortcuts:
+
+- Decks (default "d")
+- Overview (default "s")
+- Add (default "a")
+- Browse (default "b")
+- Stats (default "S")
+- Sync (default "y")
+
+# Support
+
+Post a [new issue on Github](https://github.com/Arthaey/anki-custom-keyboard-shortcuts/issues/new)
+(or make a pull request!).
+
+[My other Anki add-ons](https://github.com/search?q=user%3AArthaey+anki)
+are also on Github.
+
+# TODO
+
+- update tooltip help text
+- allow customizing of other shortcuts
+
+# License
+
+This addon is licensed under the same license as Anki itself (GNU Affero General
+Public License 3).
